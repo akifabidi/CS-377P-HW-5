d1128e2df4503722e473889776ca24dfc6c63de4
Akif Abidi <akifa@Akifs-MacBook-Air.local>
2022-11-03 12:30:17 -0500

    updated

--- pi.c
+++ pi.c
@@ -1,10 +1,10 @@
-#include <pthread.h> /*used in other parts of the assignment */
-#include <stdlib.h>
 #include <math.h>
+#include <pthread.h>   /*used in other parts of the assignment */
+#include <stdatomic.h> /* used in other parts of the assignment */
+#include <stdint.h>    /* for uint64  */
 #include <stdio.h>
-#include <stdint.h> /* for uint64  */
-#include <time.h>   /* for clock_gettime */
-#include <atomic>   /*used in other parts of the assignment */
+#include <stdlib.h>
+#include <time.h> /* for clock_gettime */
 
 double f_part1(double x)
 {
@@ -24,7 +24,7 @@ void part1()
 
     clock_gettime(CLOCK_MONOTONIC_RAW, &tick);
 
-    double x = 0.0d;
+    double x = 0.0;
     for (int i = 0; i < numPoints; i++)
     {
         pi = pi + step * f_part1(x); // Add to local sum
@@ -44,9 +44,9 @@ void part1()
 //--------------------------------------------------------------
 
 
-double f_part2(double x)
+double f_unit_circle(double x)
 {
-    return (6.0 / sqrt(1 - x * x));
+    return (sqrt(1 - x * x));
 }
 
 void part2()
@@ -54,13 +54,14 @@ void part2()
     uint64_t execTime; /*time in nanoseconds */
     struct timespec tick, tock;
 
+    double pi = 0.0;
     int numPoints = 1000000000;
     double step = 0.5 / numPoints;
 
     clock_gettime(CLOCK_MONOTONIC_RAW, &tick);
 
-    double x = 0.0d;
-    for (int i = 0; i < numPoints; i++)
+    double x = 0.0;
+    for (int i = -0.5; i < numPoints; i++)
     {
         pi = pi + step * f(x); // Add to local sum
         x = x + step;          // next x
@@ -75,6 +76,35 @@ void part2()
     printf("%.20f\n", pi);
     return 0;
 }
+
+void part3()
+{
+    uint64_t execTime; /*time in nanoseconds */
+    struct timespec tick, tock;
+
+    double pi = 0.0;
+    int numPoints = 1000000000;
+    double step = 0.5 / numPoints;
+
+    clock_gettime(CLOCK_MONOTONIC_RAW, &tick);
+
+    double x = 0.0;
+    for (int i = -0.5; i < numPoints; i++)
+    {
+        pi = pi + step * f(x); // Add to local sum
+        x = x + step;          // next x
+    }
+
+    clock_gettime(CLOCK_MONOTONIC_RAW, &tock);
+
+    execTime = 1000000000 * (tock.tv_sec - tick.tv_sec) + tock.tv_nsec - tick.tv_nsec;
+
+    printf("elapsed process CPU time = %llu nanoseconds\n", (long long unsigned int)execTime);
+
+    printf("%.20f\n", pi);
+    return 0;
+}
+
 
 int main(int argc, char *argv[])
 {
